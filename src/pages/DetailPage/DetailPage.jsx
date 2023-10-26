403fcc89ce72f708b846531978cd4b02f24ce9dc
qingge-yu <maggieqgg@gmail.com>
2023-10-26 15:13:26 -0700

    finishes functionality and styling for home page and detail page

--- src/pages/DetailPage/DetailPage.jsx
+++ src/pages/DetailPage/DetailPage.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react'
-import { useParams } from 'react-router-dom'
+import { useParams, Link } from 'react-router-dom'
 import { getbusinesses } from '../../api/yelp_api'
 import './DetailPage.css'
 
@@ -13,7 +13,15 @@ export default function DetailPage({ data }) {
 
     return (
         <>
-            <h1 className="detailTitle">{myChoice[0].name}</h1>
+            <div className="detailPageTop">
+                <Link to='/'><svg id="goBackIcon" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 19.5L8.25 12l7.5-7.5" />
+                </svg></Link>
+                <h1 className="detailTitle">{myChoice[0].name}</h1>
+                <svg id='favIconBig' xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z" />
+                </svg>
+            </div>
             <div className="detailCard">
                 < img src={myChoice[0].image_url} alt="image" />
                 <h3>{myChoice[0].name}</h3>
@@ -21,8 +29,9 @@ export default function DetailPage({ data }) {
                 <p>{myChoice[0].price}</p>
                 <p>{myChoice[0].location.address1}, {myChoice[0].location.address2}, {myChoice[0].location.city}, {myChoice[0].location.zip_code}</p>
                 <p>{myChoice[0].phone}</p>
-                <button id="categories">{myChoice[0].categories[0].title}</button>
+                <button id="categoriesDetail">{myChoice[0].categories[0].title}</button>
             </div>
+            <a href={`${myChoice[0].url}`} id='yelpBtn' target='_blank'>OPEN IN YELP!</a>
         </>
     )
 }
