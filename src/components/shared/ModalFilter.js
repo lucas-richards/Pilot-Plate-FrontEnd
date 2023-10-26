e3eceefea026514fc11e0403c53b96e8aaa570cf
lucas richards dev <lucasrichardsdev@gmail.com>
2023-10-26 09:55:01 -0700

    add icons

--- src/components/shared/ModalFilter.js
+++ src/components/shared/ModalFilter.js
@@ -53,18 +53,19 @@ function ModalFilter({
 
 	return (
 		<>
-			<Button>
-				<svg
-					fill="currentColor"
-					viewBox="0 0 16 16"
-					className='w-16 h-16'
-					onClick={handleShow}
-					>
-					<path d="M6 10.5a.5.5 0 01.5-.5h3a.5.5 0 010 1h-3a.5.5 0 01-.5-.5zm-2-3a.5.5 0 01.5-.5h7a.5.5 0 010 1h-7a.5.5 0 01-.5-.5zm-2-3a.5.5 0 01.5-.5h11a.5.5 0 010 1h-11a.5.5 0 01-.5-.5z" />
-				</svg>
-				
-			</Button>
-				
+			
+				<Button onClick={handleShow} variant="" >
+					<svg
+						fill="currentColor"
+						viewBox="0 0 16 16"
+						height="2em"
+						width="2em"
+						
+						>
+						<path d="M6 10.5a.5.5 0 01.5-.5h3a.5.5 0 010 1h-3a.5.5 0 01-.5-.5zm-2-3a.5.5 0 01.5-.5h7a.5.5 0 010 1h-7a.5.5 0 01-.5-.5zm-2-3a.5.5 0 01.5-.5h11a.5.5 0 010 1h-11a.5.5 0 01-.5-.5z" />
+					</svg>
+				</Button>
+			
 
 			<Modal show={show} onHide={handleClose}>
 				<Modal.Header closeButton>
